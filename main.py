bd4a3df303da8f3cf24f40ba9603bb1d69e5f723
Daniil Leshchev <leshev.da@gmail.com>
2024-08-26 22:31:53 +0500

    get_matches function and quickstart with gcal

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ from dateutil.relativedelta import relativedelta
 import datetime
 
 FAV_TEAM = 'Real Madrid'
-page = req.get('https://www.skysports.com/real-madrid-fixtures')
-page_parsed = BeautifulSoup(page.content, 'html.parser')
-matches = page_parsed.find_all('div', class_='fixres__item')
-
 now = datetime.datetime.now()
 next_month = f'0{now.month + 1}'
+
+def get_matches():
+    page = req.get('https://www.skysports.com/real-madrid-fixtures')
+    page_parsed = BeautifulSoup(page.content, 'html.parser')
+    return page_parsed.find_all('div', class_='fixres__item')
 
 def get_match_datetime(match):
     date = match.find_previous_sibling('h4', class_='fixres__header2').text.strip()
@@ -28,20 +29,18 @@ def get_opponent(match, fav_team):
     if teams[0].text.strip() == fav_team:
         return teams[1].text.strip()
     return teams[0].text.strip()
-    
-def create_events_list(matches):
-    match_list = []
-    for match in matches:
+
+def create_events_list():
+    matches_list = []
+    for match in get_matches():
         date = get_match_datetime(match)
         if date[0][5:7] != next_month:
             continue
 
-        match_list.append({
+        matches_list.append({
             'summary': f'{FAV_TEAM} vs {get_opponent(match, FAV_TEAM)}',
             'start_time': date[0],
             'end_time': date[1]
         })
 
-    return match_list
-
-print(create_events_list(matches))
+    return matches_list
